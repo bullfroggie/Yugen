0b49e2cb6f81b551bbb2fb27bd3c97361e8fd41f
Nikola Lausev <lausevnikola@protonmail.com>
2020-01-05 23:35:18 +0100

    add registration and login route

--- static/app/app.js
+++ static/app/app.js
@@ -1,22 +1,25 @@
 (function(angular) {
-    let app = angular.module("app", ["ui.router", "ui.bootstrap"]);
+	let app = angular.module("app", ["ui.router", "ui.bootstrap"]);
 
-    app.config(["$stateProvider", "$urlRouterProvider", function($stateProvider, $urlRouterProvider) {
-        $stateProvider
-        .state("register", {
-            url: "/register",
-            templateUrl: "app/components/auth/register.tpl.html",
-            controller: "AuthCtrl",
-            controllerAs: "auth"
-        })
-        .state("login", {
-            url: "/login",
-            templateUrl: "app/components/auth/login.tpl.html",
-            controller: "AuthCtrl",
-            controllerAs: "auth"
-        });
+	app.config([
+		"$stateProvider",
+		"$urlRouterProvider",
+		function($stateProvider, $urlRouterProvider) {
+			$stateProvider
+				.state("register", {
+					url: "/register",
+					templateUrl: "app/components/auth/register.tpl.html",
+					controller: "AuthCtrl",
+					controllerAs: "auth"
+				})
+				.state("login", {
+					url: "/login",
+					templateUrl: "app/components/auth/login.tpl.html",
+					controller: "AuthCtrl",
+					controllerAs: "auth"
+				});
 
-        $urlRouterProvider.otherwise("/register");
-    }]);
-
+			$urlRouterProvider.otherwise("/register");
+		}
+	]);
 })(angular);
