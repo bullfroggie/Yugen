749d47939025910d875813e7376f7d2117199a4e
Nikola Lausev <lausevnikola@protonmail.com>
2020-01-22 00:47:48 +0100

    add ngCookies as dependency

--- static/app/app.js
+++ static/app/app.js
@@ -1,25 +1,37 @@
 (function(angular) {
-	let app = angular.module("app", ["ui.router", "ui.bootstrap"]);
+    let app = angular.module("app", ["ui.router", "ui.bootstrap", "ngCookies"]);
 
-	app.config([
-		"$stateProvider",
-		"$urlRouterProvider",
-		function($stateProvider, $urlRouterProvider) {
-			$stateProvider
-				.state("register", {
-					url: "/register",
-					templateUrl: "app/components/auth/register.tpl.html",
-					controller: "AuthCtrl",
-					controllerAs: "auth"
-				})
-				.state("login", {
-					url: "/login",
-					templateUrl: "app/components/auth/login.tpl.html",
-					controller: "AuthCtrl",
-					controllerAs: "auth"
-				});
+    app.config([
+        "$stateProvider",
+        "$urlRouterProvider",
+        function($stateProvider, $urlRouterProvider) {
+            $stateProvider
+                .state("register", {
+                    url: "/register",
+                    templateUrl: "app/components/auth/register.tpl.html",
+                    controller: "AuthCtrl",
+                    controllerAs: "auth"
+                })
+                .state("login", {
+                    url: "/login",
+                    templateUrl: "app/components/auth/login.tpl.html",
+                    controller: "AuthCtrl",
+                    controllerAs: "auth"
+                })
+                .state("user_homepage", {
+                    url: "/",
+                    templateUrl: "app/components/nav/dashboard.tpl.html",
+                    controller: "UserCtrl",
+                    controllerAs: "uctrl"
+                })
+                .state("admin_homepage", {
+                    url: "/admin",
+                    templateUrl: "app/components/nav/dashboard.tpl.html",
+                    controller: "AdminCtrl",
+                    controllerAs: "actrl"
+                });
 
-			$urlRouterProvider.otherwise("/register");
-		}
-	]);
+            $urlRouterProvider.otherwise("/register");
+        }
+    ]);
 })(angular);
