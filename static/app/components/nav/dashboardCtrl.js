aaf0ff6c2d9489b7df377ec943483de7c36d5983
Nikola Lausev <lausevnikola@protonmail.com>
2020-01-23 23:35:38 +0100

    Fix dashboard

---
+++ static/app/components/nav/dashboardCtrl.js
@@ -0,0 +1,60 @@
+(function(angular) {
+    let app = angular.module("app");
+
+    app.controller("DashboardCtrl", [
+        "$http",
+        "$state",
+        "$cookies",
+        function($http, $state, $cookies) {
+            let that = this;
+
+            if (!$cookies.get("yugen_user")) {
+                $state.go("login");
+            }
+
+            this.loggedIn = {
+                id: null,
+                user_types_id: null,
+                first_name: "",
+                last_name: "",
+                profile_image: ""
+            };
+
+            this.getUser = function() {
+                $http.get("api/user").then(
+                    function(response) {
+                        console.log(response);
+                        that.loggedIn = {
+                            id: response.data.id,
+                            user_types_id: response.data.user_types_id,
+                            first_name: response.data.first_name,
+                            last_name: response.data.last_name,
+                            profile_image: response.data.profile_image
+                        };
+
+                        if (response.data.user_types_id == 1) {
+                            $state.go("dashboard.admin_home");
+                        }
+                    },
+                    function(reason) {
+                        console.log(reason);
+                    }
+                );
+            };
+
+            this.userLogout = function() {
+                $http.post("api/user/logout").then(
+                    function(response) {
+                        console.log(response);
+                        $state.go("login");
+                    },
+                    function(reason) {
+                        console.log(reason);
+                    }
+                );
+            };
+
+            this.getUser();
+        }
+    ]);
+})(angular);
