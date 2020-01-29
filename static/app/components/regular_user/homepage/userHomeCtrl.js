513ac6aa0e4f75ed50b402773d4e985545c6b206
Nikola Lausev <lausevnikola@protonmail.com>
2020-01-29 21:58:14 +0100

    Finish regular user homepage (both controller and view)

---
+++ static/app/components/regular_user/homepage/userHomeCtrl.js
@@ -0,0 +1,10 @@
+(function(angular) {
+    let app = angular.module("app");
+
+    app.controller("UserHomeCtrl", [
+        "$http",
+        function($http) {
+            let that = this;
+        }
+    ]);
+})(angular);
